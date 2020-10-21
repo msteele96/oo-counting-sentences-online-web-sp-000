a2b5b42bd76330a963dbf9ea9dee963bdd92e557
Marc Steele <marcsteele2014@gmail.com>
2020-10-21 17:06:28 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter_array = (self.split(/[.?!]+/))
+    return counter_array.length
   end
 end
